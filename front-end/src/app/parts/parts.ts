c0b966a9345c7c9d3fccb619c1c42b0b2b2f6963
Oscar Fortuny <oscarfortuny@gmail.com>
2025-12-09 18:56:55 +0100

    feat: add header, footer, and parts components with basic structure and tests

---
+++ front-end/src/app/parts/parts.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parts',
+  imports: [],
+  templateUrl: './parts.html',
+  styleUrl: './parts.css',
+})
+export class Parts {
+
+}
