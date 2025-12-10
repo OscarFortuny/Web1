c531ce4c9870d5dec7f8788b31d27aa97b111c8d
Oscar Fortuny <oscarfortuny@gmail.com>
2025-12-10 10:43:00 +0100

    fix: correct profile component import in routing configuration

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
 import { About } from './pages/about/about';
 import { profile } from 'console';
+import { Profile } from './pages/profile/profile';
 
 export const routes: Routes = [
   {path: '', component: Home },
   {path: 'contacte', component: Contact },
   {path:'nosaltres', component: About },
-  {path:'perfil', component: profile },
+  {path:'perfil', component: Profile },
   {path:'**', redirectTo: '' }
 ];
