e3b4a9abafe943c0d6bc7dd1615cca72094d8093
Oscar Fortuny <oscarfortuny@gmail.com>
2025-12-10 10:42:06 +0100

    feat: update routing configuration to include profile page

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
+import { About } from './pages/about/about';
+import { profile } from 'console';
 
 export const routes: Routes = [
   {path: '', component: Home },
+  {path: 'contacte', component: Contact },
+  {path:'nosaltres', component: About },
+  {path:'perfil', component: profile },
+  {path:'**', redirectTo: '' }
 ];
