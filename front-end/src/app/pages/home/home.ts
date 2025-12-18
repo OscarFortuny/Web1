b61e93c71576ce930ae909c978e531d4bf0b921f
bielgregori <bielgregori.2005@gmail.com>
2025-12-18 19:37:25 +0100

    borro la entitat de films que vam fer servir de exemple

--- front-end/src/app/pages/home/home.ts
+++ front-end/src/app/pages/home/home.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { UniversitatsService } from '../../services/universitats-service';
+import { Grups } from '../../services/grups';
+import { Usuaris } from '../../services/usuaris';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +13,44 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
-  constructor(public authService: AuthService) {}
+export class Home implements OnInit {
+  // Estadístiques
+  totalUniversitats = signal<number>(0);
+  totalGrups = signal<number>(0);
+  totalEstudiants = signal<number>(0);
+  totalPaisos = signal<number>(0);
+
+  constructor(
+    public authService: AuthService,
+    private universitatsService: UniversitatsService,
+    private grupsService: Grups,
+    private usuarisService: Usuaris
+  ) {}
+
+  ngOnInit() {
+    this.loadStats();
+  }
+
+  loadStats() {
+    forkJoin({
+      universitats: this.universitatsService.getUniversitats(),
+      grups: this.grupsService.getGrups(),
+      usuaris: this.usuarisService.getUsuaris()
+    }).subscribe({
+      next: (data) => {
+        this.totalUniversitats.set(data.universitats.length);
+        this.totalGrups.set(data.grups.length);
+        this.totalEstudiants.set(data.usuaris.length);
+
+        // Comptar països únics de les universitats
+        const paisos = new Set(data.universitats.map(u => u.country));
+        this.totalPaisos.set(paisos.size);
+      },
+      error: (err) => {
+        console.error('Error carregant estadístiques:', err);
+      }
+    });
+  }
 
   userHasGroup(): boolean {
     const user = this.authService.currentUser();
